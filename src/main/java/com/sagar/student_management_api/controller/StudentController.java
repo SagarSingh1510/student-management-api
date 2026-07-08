0dfc9584437c27a7dae21a5a585262bdd730f532
Sagar Singh <sagariscool002@gmail.com>
2026-07-08 18:53:17 +0530

    feat: add students endpoint returning JSON list

--- src/main/java/com/sagar/student_management_api/controller/StudentController.java
+++ src/main/java/com/sagar/student_management_api/controller/StudentController.java
@@ -1,6 +1,12 @@
 package com.sagar.student_management_api.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import com.sagar.student_management_api.model.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -19,6 +25,18 @@ public class StudentController {
     public String hello() {
         return "Hello Sagar!";
     }
+    @GetMapping("/students")
+        public List<Student> student(){
+            List<Student> students=new ArrayList<>();
+            Student s1=new Student(1, "Sagar", "singh@gmail.com");
+            students.add(s1);
+            Student s2=new Student(2, "Harsh", "singh1@gmail.com");
+            students.add(s2);
+            Student s3=new Student(3, "Pawan", "singh2@gmail.com");
+            students.add(s3);
+            return students;
+        }
+    
     
     
 }
