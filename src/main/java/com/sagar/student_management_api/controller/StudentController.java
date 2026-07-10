b2c35cf6d8c255d583c3690e033fe0dfa4915619
Sagar Singh <sagariscool002@gmail.com>
2026-07-10 18:25:52 +0530

    feat: add create and update student endpoint

--- src/main/java/com/sagar/student_management_api/controller/StudentController.java
+++ src/main/java/com/sagar/student_management_api/controller/StudentController.java
@@ -3,16 +3,25 @@ package com.sagar.student_management_api.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sagar.student_management_api.model.Student;
+import com.sagar.student_management_api.services.StudentService;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @RestController
 public class StudentController {
+    private final StudentService studentService;
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
     @GetMapping("/")
     public String home() {
         return "Welcome to Student Management API!";
@@ -26,16 +35,19 @@ public class StudentController {
         return "Hello Sagar!";
     }
     @GetMapping("/students")
-        public List<Student> student(){
-            List<Student> students=new ArrayList<>();
-            Student s1=new Student(1, "Sagar", "singh@gmail.com");
-            students.add(s1);
-            Student s2=new Student(2, "Harsh", "singh1@gmail.com");
-            students.add(s2);
-            Student s3=new Student(3, "Pawan", "singh2@gmail.com");
-            students.add(s3);
-            return students;
+        public List<Student> students(){
+            return studentService.getStudents();
         }
+
+    @GetMapping("/students/{id}")
+    public Student getStudentById(@PathVariable int id) {
+        return studentService.getStudentById(id);
+    }
+    
+    @PostMapping("/students")
+    public Student addStudent(@RequestBody Student student) {
+        return studentService.addStudent(student);
+    }
     
     
     
--- src/main/java/com/sagar/student_management_api/services/StudentService.java
+++ src/main/java/com/sagar/student_management_api/services/StudentService.java
@@ -1,8 +1,46 @@
 package com.sagar.student_management_api.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.sagar.student_management_api.model.Student;
 
 @Service
 public class StudentService {
-    
+    private List<Student> students= new ArrayList<>();
+    public StudentService(){
+        students.add(new Student(1, "Sagar", "singh@gmail.com"));
+        students.add(new Student(2, "Harsh", "singh1@gmail.com"));
+        students.add(new Student(3, "Pawan", "singh2@gmail.com"));
+    }
+    public List<Student> getStudents(){
+        return students;
+    }
+
+    public Student getStudentById(int id){
+        for(Student student: students){
+            if(student.getId()==id){
+                return student;
+            }
+        }
+        return null;
+    }
+
+    public Student addStudent(Student student){
+        students.add(student);
+        return students.getLast();
+    }
+
+    public Student updateStudent(int id,Student updatedStudent){
+        for(Student student:students){
+            if(student.getId()==id){
+                student.setName(updatedStudent.getName());
+                student.setEmail(updatedStudent.getEmail());
+                return student;
+            }
+        }
+        return null;
+    }
 }
